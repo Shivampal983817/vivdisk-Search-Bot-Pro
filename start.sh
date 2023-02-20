9f6020453480ae39df7782d654eac2bd904acc95
Rohit Kushwaha <34808268+Shivampal983817@users.noreply.github.com>
2023-02-20 21:03:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/xyz_ott/vivdisk-Search-Bot-Pro.git/vivdisk-Search-Bot-Pro
-cd /vivdisk-Search-Bot-Pro
+git clone https://github.com/xyz_ott/Link-Search-Bot-Pro /MdiskWalaBot
+cd /MdiskWalaBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
