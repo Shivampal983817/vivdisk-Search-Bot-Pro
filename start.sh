6c801d05266c477ac29157fe1e68614349f5245f
Rohit Kushwaha <34808268+Shivampal983817@users.noreply.github.com>
2023-02-20 16:29:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/xyz_ott/vivdisk-Search-Bot-Pro.git/vivdisk-Search-Bot-Pro
+cd /vivdisk-Search-Bot-Pro
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
